e0eca729404f1de7aa808c850aebbd61fbcec6cc
kgrinaldi <kgrinaldi@gmail.com>
2017-10-16 16:21:48 -0700

    Main Build for Browse Product Test

--- src/test/java/HW/Work/AppTestThree.java
+++ src/test/java/HW/Work/AppTestThree.java
@@ -1,5 +1,39 @@
 package HW.Work;
 
-public class AppTestThree {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+public class AppTestThree {
+	public String website = "http://automationpractice.com/index.php";
+	public WebDriver chrome;
+	
+	
+	@BeforeClass
+	public void Before() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kgrin\\Documents\\chromedriver.exe");
+		chrome = new ChromeDriver();
+		System.out.println("Your journey has begun");
+	}
+
+	@Test(groups = {"p1", "Sign In"})
+	public void testSignIn() {
+		chrome.get(website);
+
+		//Click Link to View a Product
+		WebElement btnProduct = chrome.findElement(By.linkText("Faded Short Sleeve T-shirts"));
+		btnProduct.click();
+	
+	}
+	
+	@AfterClass
+	public void After() {
+		chrome.quit();
+		System.out.println("You just saved the princess! Your journey has ended.");
+	}
+	
 }
