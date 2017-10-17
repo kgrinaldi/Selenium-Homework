368d9c941d7ef58c083a0ce0c22229e8dc0e9348
kgrinaldi <kgrinaldi@gmail.com>
2017-10-16 18:14:13 -0700

    Final Build of Search Engine Test

---
+++ src/test/java/HW/Work/AppTestFive.java
@@ -0,0 +1,46 @@
+package HW.Work;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AppTestFive {
+	public String website = "http://automationpractice.com/index.php";
+	public WebDriver chrome;
+	
+	@BeforeClass
+	public void Before() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\kgrin\\Documents\\chromedriver.exe");
+		chrome = new ChromeDriver();
+		System.out.println("Your journey has begun");
+	}
+	
+	@Test(groups = {"p1", "Add to Cart"})
+	public void testSearch() {
+		chrome.get(website);
+		
+		//Test the Search Engine
+		WebElement txtSearch = chrome.findElement(By.id("search_query_top"));
+		WebElement btnSearch = chrome.findElement(By.name("submit_search"));
+		
+		txtSearch.sendKeys("white blouse");
+		btnSearch.click();
+		
+		txtSearch = chrome.findElement(By.id("search_query_top"));
+		btnSearch = chrome.findElement(By.name("submit_search"));
+		
+		txtSearch.clear();
+		txtSearch.sendKeys("orange shirt");
+		btnSearch.click();
+	}
+	
+	@AfterClass
+	public void After() {
+		chrome.quit();
+		System.out.println("You just saved the princess! Your journey has ended.");
+	}
+}
